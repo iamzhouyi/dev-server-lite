e1c73099fab12f9718d5ee264e19bd259dddd6bf
周轶 <zhouyi@zhouyis-iMac.local>
2018-04-21 20:48:15 +0800

    支持配置项

--- index.js
+++ index.js
@@ -1,31 +1,46 @@
-const finalhandler = require('finalhandler')
-const http = require('http')
-const serveStatic = require('serve-static')
-const httpProxy = require('http-proxy')
-const HttpProxyRules = require('http-proxy-rules')
+#!/usr/bin/env node
+
+var finalhandler = require('finalhandler');
+var http = require('http');
+var serveStatic = require('serve-static');
+var httpProxy = require('http-proxy')
+var HttpProxyRules = require('http-proxy-rules')
+var fs = require('fs');
+var path = require('path');
+
+var { root } = getCmdLineArgv()
+var configFilePath = './proxy-rules.json'
+
+var defaultProxyOptions = { changeOrigin: true, ws: false}
+var defaultRules = {}
+
+let userProxyOptions = {}, userRules = {}, port = 3000
+
+if (fs.existsSync(configFilePath)){
+    var config = JSON.parse(fs.readFileSync(configFilePath, 'utf8'));
+    userProxyOptions = config.proxyOptions || {}
+    userRules = config.rules || {}
+    port = config.port || 3000
+}
 
 // For rules see https://github.com/donasaur/http-proxy-rules
-const proxyRules = new HttpProxyRules({
-    rules: {
-      '/api': 'http://127.0.0.1/api',
-    },
+var proxyRules = new HttpProxyRules({
+    rules: Object.assign({}, defaultRules, userRules)
 });
 
 // For options see https://github.com/nodejitsu/node-http-proxy
-const proxyOptions = {
-    changeOrigin: true,
-    ws: true,
-}
+var proxyOptions = Object.assign({}, defaultProxyOptions, userProxyOptions)
 
-const port = 3000
+console.log('proxyRules', proxyRules)
+console.log('proxyOptions', proxyOptions)
 
-const proxy = httpProxy.createProxy(proxyOptions);
+var proxy = httpProxy.createProxy(proxyOptions);
 
 // Serve static resource
-const serve = serveStatic('./', {'index': ['index.html', 'index.htm']})
+var serve = serveStatic(root, {'index': ['index.html', 'index.htm']})
 
 // Create server
-const server = http.createServer(function onRequest (req, res) {
+var server = http.createServer(function onRequest (req, res) {
     var target = proxyRules.match(req);
     if (target) {
         return proxy.web(req, res, {
@@ -35,4 +50,19 @@ const server = http.createServer(function onRequest (req, res) {
     serve(req, res, finalhandler(req, res))
 })
 
+console.log('dev-server started port=' + port + ', root=' + root)
 server.listen(port)
+
+
+function getCmdLineArgv() {
+    var config = {
+        root: './'
+    }
+    var argvs = process.argv
+    for (let i = 0; i< argvs.length ; i++) {
+        if(argvs[i] === '--root'){
+            config.root = argvs[i+1]
+        }
+    }
+    return config;
+}
